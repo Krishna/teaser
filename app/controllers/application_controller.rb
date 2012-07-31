e0d25d9e40b95828bba011756351798b5d28dcd8
Krishna Kotecha <krishna.kotecha@logiccolony.com>
2012-08-01 00:48:35 +0100

    Add authenticate method, and set it up to be usable on Heroku via environment vars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      actual_password = ENV['ADMIN_PASSWORD'] || "password"
+      actual_username = ENV['ADMIN_USERNAME'] || "username"
+      username == "username" && password == "password"
+    end
+  end
+
 end
